6a3d2a0bf13ce9bcfdd99e2cfaf2cd0c98f79ab7
Burcin Sayin <burcinsayin@gmail.com>
2025-08-12 08:40:46 +0300

    use profile for exports

--- config/exports.sh
+++ config/exports.sh
@@ -1,3 +1,8 @@
 #
 # Example export:
 # export EDITOR=vim
+
+# Add Flutter SDK to PATH if it exists
+if [ -d "/media/orin/TuxData/work/SDK/flutter/bin" ] && [ "$(ls -A "/media/orin/TuxData/work/SDK/flutter/bin")" ]; then
+  export PATH="/media/orin/TuxData/work/SDK/flutter/bin:$PATH"
+fi
--- setup.sh
+++ setup.sh
@@ -5,15 +5,27 @@
 # This script's directory, for a robust path
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 ALIASES_FILE="$SCRIPT_DIR/config/aliases.sh"
+EXPORTS_FILE="$SCRIPT_DIR/config/exports.sh"
 BASHRC_FILE=~/.bashrc
+BASH_PROFILE_FILE=~/.bash_profile
 
-SOURCE_LINE="source \"$ALIASES_FILE\""
+SOURCE_ALIAS_LINE="source \"$ALIASES_FILE\""
+SOURCE_EXPORT_LINE="source \"$EXPORTS_FILE\""
 
 # Add sourcing of aliases to ~/.bashrc if not already present
-if ! grep -Fxq "$SOURCE_LINE" "$BASHRC_FILE"; then
+if ! grep -Fxq "$SOURCE_ALIAS_LINE" "$BASHRC_FILE"; then
     echo "Adding aliases to $BASHRC_FILE"
-    printf "\n# Source shell-config aliases\n%s\n" "$SOURCE_LINE" >> "$BASHRC_FILE"
+    printf "\n# Source shell-config aliases\n%s\n" "$SOURCE_ALIAS_LINE" >> "$BASHRC_FILE"
     echo "Done. Please run 'source $BASHRC_FILE' or open a new terminal."
 else
     echo "Aliases already configured in $BASHRC_FILE."
+fi
+
+# Add sourcing of exports to ~/.bash_profile if not already present
+if ! grep -Fxq "$SOURCE_EXPORT_LINE" "$BASH_PROFILE_FILE"; then
+    echo "Adding exports to $BASH_PROFILE_FILE"
+    printf "\n# Source shell-config exports\n%s\n" "$SOURCE_EXPORT_LINE" >> "$BASH_PROFILE_FILE"
+    echo "Done. Please open a new terminal to see the changes."
+else
+    echo "Exports already configured in $BASH_PROFILE_FILE."
 fi
