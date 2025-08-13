1fdcf249e18099055bf425dc834d6834afa2f044
Burcin Sayin <burcinsayin@gmail.com>
2025-08-13 15:23:16 +0300

    android

--- config/exports.sh
+++ config/exports.sh
@@ -6,3 +6,9 @@
 if [ -d "$HOME/Android/flutter/bin" ] && [ "$(ls -A "$HOME/Android/flutter/bin")" ]; then
   export PATH="$HOME/Android/flutter/bin:$PATH"
 fi
+
+
+# Add Android SDK platfor tools to PATH if it exists
+if [ -d "$HOME/Android/Sdk/platform-tools" ] && [ "$(ls -A "$HOME/Android/Sdk/platform-tools")" ]; then
+  export PATH="$HOME/Android/Sdk/platform-tools:$PATH"
+fi
